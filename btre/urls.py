2b0773f09b5a01afc7bd12e5751f0e68f97754dc
Ritika Upadhyay <ritikaupadhyay.me@gmail.com>
2020-04-24 17:16:58 -0500

    adding in elements of the theme

--- btre/urls.py
+++ btre/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
 ]
-    
+
+urlpatterns += staticfiles_urlpatterns()
--- pages/views.py
+++ pages/views.py
@@ -3,8 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def index(request):
     return render(request, 'pages/index.html')
+
 
 def about(request):
     return render(request, 'pages/about.html')
