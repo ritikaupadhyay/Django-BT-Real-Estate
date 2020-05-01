d14a584e3da6976b29c720c39c06472d3a9ca150
Ritika Upadhyay <ritikaupadhyay.me@gmail.com>
2020-05-01 16:33:17 -0500

    Connect view to underlying database

--- listings/views.py
+++ listings/views.py
@@ -1,17 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listing
 
 
 def index(request):
-    listings = Listing.objects.all()
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
 
-    context = {'listings': listings}
+    paginator = Paginator(listings, 6)
+    page = request.GET.get('page')
+    page_listings = paginator.get_page('page')
+
+    context = {'listings': page_listings}
 
     return render(request, 'listings/listings.html', context)
 
 
-def listing(request):
-    return render(request, 'listings/listing.html')
+def listing(request, listing_id):
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {'listing': listing}
+    return render(request, 'listings/listing.html', context)
 
 
 def search(request):
