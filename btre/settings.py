1db46ef91bc130eda04ec9221a58c6e759cfc741
Ritika Upadhyay <ritikaupadhyay.me@gmail.com>
2020-05-05 17:37:39 -0500

    change to settings file in btre

--- btre/settings.py
+++ btre/settings.py
@@ -141,3 +141,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'finguhguns'
 EMAIL_HOST_PASSWORD = 'etqymyveibvtnvyw'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
